0be55f1bd1dd143a637e523e890cc2956cf4c4d4
David Bourgin <ddbourgin@gmail.com>
2022-02-05 18:32:23 -0500

    update install instructions

---
+++ clone_zsh_repos.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
