e4931fe06932d1e31bd12da4e4e82ca9f04f5d83
David Bourgin <ddbourgin@users.noreply.github.com>
2022-08-12 16:51:29 -0400

    Update clone_zsh_repos.sh

--- clone_zsh_repos.sh
+++ clone_zsh_repos.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
 git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
+git clone git@github.com:ycm-core/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
